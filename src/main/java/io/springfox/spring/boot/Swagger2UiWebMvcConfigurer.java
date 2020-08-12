2671b3935e9467a63ac6bbdc479c489bc7c34dc8
wandl <hiwepy@163.com>
2020-08-12 12:31:32 +0800

    优化逻辑

--- src/main/java/io/springfox/spring/boot/Swagger2UiWebMvcConfigurer.java
+++ src/main/java/io/springfox/spring/boot/Swagger2UiWebMvcConfigurer.java
@@ -1,29 +1,29 @@
 package io.springfox.spring.boot;
 
-import org.springframework.util.StringUtils;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.resource.WebJarsResourceResolver;
 
 public class Swagger2UiWebMvcConfigurer implements WebMvcConfigurer {
-	  private final String baseUrl;
 
-	  public Swagger2UiWebMvcConfigurer(String baseUrl) {
-	    this.baseUrl = baseUrl;
-	  }
-
-	  @Override
-	  public void addResourceHandlers(ResourceHandlerRegistry registry) {
-	    String baseUrl = StringUtils.trimTrailingCharacter(this.baseUrl, '/');
-	    registry.
-	        addResourceHandler(baseUrl + "/swagger-ui/**")
-	        .addResourceLocations("classpath:/META-INF/resources/webjars/springfox-swagger-ui/")
-	        .resourceChain(false);
-	  }
-
-	  @Override
-	  public void addViewControllers(ViewControllerRegistry registry) {
-	    registry.addViewController(baseUrl + "/swagger-ui/")
-	        .setViewName("forward:" + baseUrl + "/swagger-ui/index.html");
-	  }
+	private final String META_INF_RESOURCES = "classpath:/META-INF/resources/";
+	private final String META_INF_WEBJAR_RESOURCES = META_INF_RESOURCES + "webjars/";
+	private final String META_INF_SPRINGFOX_SWAGGER_UI_RESOURCES = META_INF_WEBJAR_RESOURCES + "springfox-swagger-ui/";
+	
+	@Override
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+		registry.addResourceHandler("/swagger-ui/**").addResourceLocations(META_INF_SPRINGFOX_SWAGGER_UI_RESOURCES).resourceChain(false);
+		registry.addResourceHandler("/doc.html**").addResourceLocations(META_INF_RESOURCES).resourceChain(false);;
+		if(!registry.hasMappingForPattern("/webjars/**")) {
+			registry.addResourceHandler("/webjars/**").addResourceLocations(META_INF_WEBJAR_RESOURCES)
+				.resourceChain(false).addResolver(new WebJarsResourceResolver());
+		}
 	}
+
+	@Override
+	public void addViewControllers(ViewControllerRegistry registry) {
+		registry.addViewController("/swagger-ui/").setViewName("forward:/swagger-ui/index.html");
+	}
+	
+}
